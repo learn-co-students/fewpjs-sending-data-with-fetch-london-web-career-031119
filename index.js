6a4d7bad69134decb801d1e2ffc9e1c181de2f12
oli-gra <oliver.grant@gmail.com>
2019-04-30 11:22:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const USERS = 'http://localhost:3000/users'
+const submitData = (name, email) => {
+   const data = {
+      name,
+      email
+   }
+   fetch(USERS,{
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json' },
+      body: JSON.stringify(data)
+   }).then( r => r.json()).then( (obj) =>  document.body.innerHTML = obj[ "id" ])
+    .catch( (error) => document.body.innerHTML = error.message)
+
+}
+const showUser = (user) => {
+   const li = document.createElement('li')
+   li.textContent = `${user.id}: ${user.name}, ${user.email}`
+   div = document.querySelector('div#users')
+   div.append(li)
+
+}
+const showUsers = () => {
+   fetch(USERS).then(r => r.json()).then((users) => users.forEach(showUser))
+}
